13b1efb1eb279b5444ba3388e84523126b2e16ca
Fabio <techbrasilia@outlook.com>
2021-01-11 21:00:00 -0300

    Desafio conceitos nodejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,90 @@ app.use(cors());
 
 const repositories = [];
 
+function middlewareValidUuid(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'ID inválido do repositório!'})
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+
+  const repository = {
+      id: uuid(), 
+      title, 
+      url, 
+      techs, 
+      likes: 0 
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", middlewareValidUuid, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repositório não encontrado'})
+  }
+
+  const repo = repositories[repositoryIndex];
+
+  const repository = {
+      id,
+      title, 
+      url, 
+      techs,
+      likes: repo.likes
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", middlewareValidUuid, (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repositório não encontrado'})
+  }
+  
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", middlewareValidUuid, (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    return response.status(400).json({ error: 'Repositório não encontrado'})
+  }
+
+  repository.likes ++;
+
+  return response.json(repository);
+
 });
 
 module.exports = app;
